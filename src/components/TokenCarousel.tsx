a5533162a2b242df513c8fe2b68aa9e0f92fa4c6
Diego Parra <diego@linkedai.co>
2025-02-04 23:59:43 +0100

    Add absolute path and create carousel elements

--- src/components/TokenCarousel.tsx
+++ src/components/TokenCarousel.tsx
@@ -1,16 +1,16 @@
 const TokenCarousel = () => {
   return (
-    <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+    <div className="carousel carousel-center space-x-4 p-4 w-full">
       <div className="carousel-item">
         <div className="card glass">
-          <figure>
+          <figure className="h-48">
             <img
               src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
               alt="POCHI"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">PEPE</h2>
+          <div className="card-body p-4">
+            <h3 className="card-title">PEPE</h3>
             <p>How to park your car at your garage?</p>
           </div>
         </div>
