f0a5c5a45aeedd5b38989adbab5cff83a95bc5aa
Diego Parra <diego@linkedai.co>
2025-02-04 22:52:33 +0100

    Add daisy framework

---
+++ src/components/TokenCarousel.tsx
@@ -0,0 +1,22 @@
+const TokenCarousel = () => {
+  return (
+    <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+      <div className="carousel-item">
+        <div className="card glass">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+              alt="POCHI"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">PEPE</h2>
+            <p>How to park your car at your garage?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TokenCarousel
