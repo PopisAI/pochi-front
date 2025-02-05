2fd7a2bbae57960f6001d85fca6e4f937ccd80ed
Diego Parra <diego@linkedai.co>
2025-02-05 22:48:40 +0100

    Create token view

--- src/pages/TokenView/index.tsx
+++ src/pages/TokenView/index.tsx
@@ -1,9 +1,65 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import Layout from '../../components/Layout'
+
+import Layout from '@/components/Layout'
+import { Token } from '@/types/Token'
+import { getTokenBySymbol } from '@/services/api'
 
 const TokenView = () => {
+  const [token, setToken] = useState<Token | null>(null)
   const { tokenId } = useParams()
-  return <Layout>TokenView: {tokenId}</Layout>
+
+  useEffect(() => {
+    if (tokenId) getToken(tokenId)
+  }, [tokenId])
+
+  const getToken = async (symbol: string) => {
+    const _tokens = await getTokenBySymbol(symbol)
+    setToken(_tokens)
+  }
+
+  return (
+    <Layout>
+      <div className="container mx-auto flex flex-col md:flex-row space-x-8">
+        <div className='flex flex-col items-center md:items-baseline'>
+          <p className="text-[12px] text-neutral-500">{token?.symbol}</p>
+          <h2 className="text-2xl mb-2">{token?.name}</h2>
+          <div className="card image-full w-64 shadow-md" style={{ display: 'block' }}>
+            <figure>
+              <img src={token?.img} alt={token?.symbol} />
+            </figure>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="card bg-neutral-800 h-83 shadow-xl mt-14">
+            <div className="card-body">
+              <h2 className="card-title">About {token?.symbol}</h2>
+              {/** Price */}
+              <div className="flex my-4">
+                <p className="text-neutral-400 text-lg font-semibold w-1">Price:</p>
+                <p className="text-lg">{`$${token?.price}`}</p>
+              </div>
+              {/** 24h */}
+              <div className="flex mb-4">
+                <p className="text-neutral-400 text-lg font-semibold w-1">24h:</p>
+                <p className="text-lg">{`${token?.h24}`}</p>
+              </div>
+              {/** Market Cap */}
+              <div className="flex mb-4">
+                <p className="text-neutral-400 text-lg font-semibold w-1">Market cap:</p>
+                <p className="text-lg">{`$${(token?.marketCap || 0)/1000000}M`}</p>
+              </div>
+              {/** Date Created */}
+              <div className="flex mb-4">
+                <p className="text-neutral-400 text-lg font-semibold w-1">Created:</p>
+                <p className="text-lg">{`5d ago`}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default TokenView
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,22 @@ import demoTokens from '@/data/tokens.json'
 import { Token } from '@/types/Token'
 
 export const getTrendingTokens = async (): Promise<Token[]> => {
-  return demoTokens.map((token) => (token as Token))
+  return demoTokens.map((token) => token as Token)
 }
 
 export const getGainerTokens = async (): Promise<Token[]> => {
-  return demoTokens.reverse().map((token) => (token as Token))
+  return demoTokens.reverse().map((token) => token as Token)
+}
+
+export const getTokenBySymbol = async (tSymbol: string): Promise<Token | null > => {
+  let token = null
+
+  for (let i = 0; i < demoTokens.length; i++) {
+    if (demoTokens[i].symbol === tSymbol) {
+      token = demoTokens[i]
+      break
+    }
+  }
+
+  return token as Token
 }
