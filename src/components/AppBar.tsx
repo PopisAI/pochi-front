adb9550ad9c9a741e53992a130fa0d5060e9e77e
Diego Parra <diego@linkedai.co>
2025-02-08 14:32:42 +0100

    Set new login view

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -8,7 +8,6 @@ const activeStyle = 'underline underline-thickness-3 underline-offset-2'
 
 const AppBar = () => {
   const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
-  console.log('🚀 ~ AppBar ~ user:', user)
   // const { data: balance, error, isPending } = useReadContract({
   //   ...wagmiContractConfig,
   //   functionName: 'balanceOf',
@@ -19,28 +18,67 @@ const AppBar = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-50 bg-stone-100/90 text-gray-900">
       <nav className="flex justify-between items-center p-4 w-full">
-        <ul className="flex items-center space-x-4">
-          <li className="font-semibold text-lg">
-            <NavLink to="/">Pochi.po</NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/tokens"
-              className={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-              Tokens
-            </NavLink>
-          </li>
-        </ul>
-        <ul className="flex items-center space-x-4">
-          <li>
-            {isAuthenticated ? (
-              <button onClick={() => logout()}>Log Out</button>
-            ) : (
-              <button onClick={() => loginWithRedirect()}>Log In</button>
-            )}
-          </li>
-        </ul>
+        <div className="navbar-start">
+          <NavLink className="font-semibold text-xl" to="/">
+            Pochi.po
+          </NavLink>
+        </div>
+        <div className="navbar-center">
+          <ul className="flex items-center space-x-4">
+            <li className="text-md">
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="text-md">
+              <NavLink
+                to="/tokens"
+                className={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Tokens
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <ul className="flex items-center space-x-4">
+            <li>
+              {isAuthenticated ? (
+                <div className="dropdown dropdown-end">
+                  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                      <img
+                        alt="User"
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                      />
+                    </div>
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-neutral-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                  >
+                    <li>
+                      <a className="justify-between">
+                        Profile
+                        <span className="badge">New</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a onClick={() => logout()}>Logout</a>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <button className="btn" onClick={() => loginWithRedirect()}>
+                  Log In
+                </button>
+              )}
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
